05c0b440f6aabdbef73b967c2db5e0e5617d6307
Egor <arara1196@gmail.com>
2020-06-26 05:40:59 +0300

    1st launch

---
+++ Controllers/PassController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mocks.Models;
+
+namespace Mocks.Controllers
+{
+    [Route("pass")]
+    [ApiController]
+    public class PassController : Controller
+    {
+        [HttpGet("{guid}")]
+        public ActionResult Get(string guid)
+        {
+            ActionResult response;
+
+            if ((response = Content(Pass.GetPass(guid))) != null)
+            {
+                return response;
+            }
+            return NotFound();
+        }
+
+    }
+}
